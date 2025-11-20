cfc2c7cd5c78f0ca265943a32f0122b4f17897f0
marceloDev0 <marcelofernandes22pm@gmail.com>
2025-11-20 17:38:28 -0300

    feat: criação do model Aluno

--- educacional/models.py
+++ educacional/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+# Importando a classe abstrata do app vizinho
+from usuarios.models import Usuario
 
-# Create your models here.
+class Aluno(Usuario):
+    # Herda nome, cpf, data_nascimento, endereco de 'Usuario'
+    
+    class StatusAluno(models.TextChoices):
+        ATIVO = 'ATIVO', 'Ativo'
+        INATIVO = 'INATIVO', 'Inativo'
+        PENDENTE_RENOVACAO = 'PENDENTE_RENOVACAO', 'Pendente Renovação'
+
+    matricula = models.CharField(max_length=20, unique=True)
+    escola = models.CharField(max_length=200)
+    
+    status_aluno = models.CharField(
+        max_length=20, 
+        choices=StatusAluno.choices,
+        default=StatusAluno.ATIVO
+    )
+
+    def __str__(self):
+        return f"{self.nome} (Mat: {self.matricula})"
