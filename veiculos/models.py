7e2040c17594e3268774f698a68add19d5223f75
Dinarte Filho <dinarte_filho@hotmail.com>
2025-11-21 10:54:33 -0300

    refactor: Simplificando o Models

--- veiculos/models.py
+++ veiculos/models.py
@@ -1,34 +1,11 @@
 from django.db import models
-from usuarios.models import Motorista
 
 class Veiculo(models.Model):
+    # Campos definidos no diagrama
+    placa = models.CharField(max_length=10, unique=True)
+    modelo = models.CharField(max_length=50)
+    capacidade = models.IntegerField()
     
-    placa = models.CharField(
-        max_length=10,
-        unique=True,
-        verbose_name="placa do veiculo"
-    )
-    
-    modelo = models.CharField(
-        max_length=50,
-        verbose_name="modelo do veiculo (exemplo: onibus, van, carro)"
-    )
 
-    capacidade = models.PositiveIntegerField(
-        verbose_name="capacidade de passageiros"
-    )
-    
-    motorista = models.ForeignKey(
-        Motorista,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        verbose_name="motorista responsavel pelo veiculo"
-    )
-    
-    class Meta:
-        verbose_name = "veiculo"
-        verbose_name_plural = "veiculos"
-        
     def __str__(self):
-        return f"{self.modelo} - {self.placa} ({self.capacidade} passageiros)"
+        return f"{self.modelo} - {self.placa}"
