3e0215bda7187c8c0fd067ecc8217580c337361d
thyagofab <thyago.fabricio2015@hotmail.com>
2025-11-23 19:33:25 -0300

    fix: ajustar caminhos do login

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = 'index.html'
+    template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
